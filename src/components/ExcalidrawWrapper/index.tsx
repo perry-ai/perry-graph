613da3f41d99ecfe0ccecd51e823f1534aa97a33
perry-ai <perry-ai@outlook.com>
2025-04-19 22:36:29 +0800

    feat: remove console

--- src/components/ExcalidrawWrapper/index.tsx
+++ src/components/ExcalidrawWrapper/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { convertToExcalidrawElements } from '@excalidraw/excalidraw'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
 import '@excalidraw/excalidraw/index.css'
@@ -23,25 +22,8 @@ const ExcalidrawWrapper: React.FC = () => {
     return null
   }
 
-  // console.info(convertToExcalidrawElements([{
-  //   type: 'rectangle',
-  //   id: 'rect-1',
-  //   x: 100,
-  //   y: 100,
-  //   width: 186.47265625,
-  //   height: 141.9765625,
-  //   strokeColor: '#000000',
-  //   backgroundColor: 'transparent',
-  //   fillStyle: 'hachure',
-  //   strokeWidth: 1,
-  //   strokeStyle: 'solid',
-  //   roughness: 1,
-  //   opacity: 100,
-  //   groupIds: [],
-  // }]))
-
   return (
-    <div style={{ height: '500px', width: '500px' }}>
+    <div style={{ height: '1000px' }}>
       <Excalidraw />
     </div>
   )
